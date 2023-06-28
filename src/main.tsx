3f71845a2790d7cae73a82ef616d32c6022be77c
hottinho <christian.erll@gmail.com>
2023-06-28 13:59:23 +0200

    "created and mounted christian page"

--- src/main.tsx
+++ src/main.tsx
@@ -5,58 +5,59 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootLayout } from "./layouts/Root";
 import { Chirag } from "./pages/Chirag";
 import { Julian } from "./pages/Julian";
+import { Christian } from "./pages/Christian";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <RootLayout />,
-		children: [
-			{
-				index: true,
-				element: <p>hello, camp10 </p>,
-			},
-			{
-				path: "chirag",
-				element: <Chirag />,
-			},
-			{
-				path: "julian",
-				element: <Julian />,
-			},
-			{
-				path: "joan",
-				element: <p>hello, joan </p>,
-			},
-			{
-				path: "pei",
-				element: <p>hello, pei </p>,
-			},
-			{
-				path: "felix",
-				element: <p>hello, felix </p>,
-			},
-			{
-				path: "jacob",
-				element: <p>hello, jacob </p>,
-			},
-			{
-				path: "christian",
-				element: <p>hello, christian</p>,
-			},
-			{
-				path: "hendryk",
-				element: <p>hello, hendryk </p>,
-			},
-			{
-				path: "tobi",
-				element: <p>hello, tobi </p>,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <p>hello, camp10 </p>,
+      },
+      {
+        path: "chirag",
+        element: <Chirag />,
+      },
+      {
+        path: "julian",
+        element: <Julian />,
+      },
+      {
+        path: "joan",
+        element: <p>hello, joan </p>,
+      },
+      {
+        path: "pei",
+        element: <p>hello, pei </p>,
+      },
+      {
+        path: "felix",
+        element: <p>hello, felix </p>,
+      },
+      {
+        path: "jacob",
+        element: <p>hello, jacob </p>,
+      },
+      {
+        path: "christian",
+        element: <Christian />,
+      },
+      {
+        path: "hendryk",
+        element: <p>hello, hendryk </p>,
+      },
+      {
+        path: "tobi",
+        element: <p>hello, tobi </p>,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
